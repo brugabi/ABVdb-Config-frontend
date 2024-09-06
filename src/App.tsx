93d3e3a9967c4fbdab68bbba87986db9eed536d3
verttb <alyssonoliveira456@gmail.com>
2024-09-06 20:44:14 -0300

    roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-
+import {BrowserRouter, Route, Routes } from "react-router-dom"
 import { Login } from "./pages/Login"
+import { Home } from "./pages/Home"
 
 
 function App() {
 
   return (
     <>
-  
-    <Login></Login>
+    <BrowserRouter>
+    <Routes>
+        <Route element={<Login></Login>} path="/login"></Route>
+        <Route element={<Home></Home> } path="/"/>
+    </Routes>
+    </BrowserRouter>
+    
     
     </>
   )
