4958cd938d05a2deccc67254a2815c0131324ffc
verttb <alyssonoliveira456@gmail.com>
2024-09-06 16:34:01 -0300

    Página de login pronta

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,8 +9,8 @@ interface inputProps{
 export const Input = ({name,type, placeholder, onChange, value}: inputProps) => {
     return(
         <>
-        <label className="text-left w-3/5  text-2xl flex-col flex gap-3" htmlFor={name}>{name}
-        <input className="h-10 p-2 border-2 border-black text-xl " value={value}required id={name}
+        <label className="text-left w-3/5 font-bold text-2xl flex-col flex gap-3" htmlFor={name}>{name}
+        <input className="h-10 p-2 border-2 font-normal border-black text-xl " value={value}required id={name}
             onChange={onChange} name={name} type={type} placeholder={placeholder}/>
         </label>
         </>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,13 @@ import { Logo } from "../components/Logo"
 
 export const Login = () => {
     return(
-
-        <div className="bg-primary w-screen h-screen flex justify-center items-center">
-            <Logo src="src/assets/g2bc.png" className="absolute bottom-0 left-8 rounded-lg"></Logo>
-            <Logo src="src/assets/logoUneb-removebg-preview.png" className="absolute right-8 rounded-lg"></Logo>
-            <div className="h-4/5 w-2/6 bg-secondary flex flex-col gap-12 text-center justify-center items-center">
+    <div className="bg-primary w-screen h-screen flex flex-col justify-around">
+        <div className="h-auto flex justify-between mt-4">
+         <Logo src="src/assets/g2bc.png" className="w-60 h-32 relative left-8 rounded-3xl"></Logo>
+         <Logo src="src/assets/logoUneb-removebg-preview.png" className=" bg-neutral relative w-60 h-32 right-8 rounded-3xl"></Logo>
+        </div>
+        <div className=" flex justify-center items-center h-4/5 w-full m-auto">
+            <div className="h-full w-2/6 bg-secondary flex flex-col gap-12 text-center justify-center items-center">
             <h2 className="font-extrabold flex items-center text-4xl gap-2">
                 <img src="src\assets\genetic-test-report-svgrepo-com.png" className="w-16"></img>
                 ABVdb&Config
@@ -16,10 +18,12 @@ export const Login = () => {
                 <img src="src\assets\login-background.jpg" alt="" />
             <h2 className="font-extrabold text-4xl">Administração</h2>
             </div>
-            <div className="h-4/5 w-2/6 bg-neutral flex flex-col text-center items-center">
+            <div className="h-full w-2/6 bg-neutral flex flex-col text-center items-center">
                 <Forms></Forms>
                
             </div>
+        </div>
+        <Footer></Footer>
         </div>
     )
 }
