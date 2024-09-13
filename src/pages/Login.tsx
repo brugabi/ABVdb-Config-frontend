c79a1c24a58c711e1380bf4d9f7e46c6a2e78c66
verttb <alyssonoliveira456@gmail.com>
2024-09-13 08:12:42 -0300

    teste

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,15 +6,15 @@ export const Login = () => {
     return(
 
         <div className="bg-primary w-screen h-screen flex justify-center items-center">
-            <Logo src="src/assets/g2bc.png" className="absolute bottom-0 left-8 rounded-lg"></Logo>
-            <Logo src="src/assets/logoUneb-removebg-preview.png" className="absolute right-8 rounded-lg"></Logo>
+            <Logo src="src/assets/g2bc.png" className="absolute bottom-0 left-8 rounded-lg top-4 w-64 h-32"></Logo>
+            <Logo src="src/assets/logoUneb-removebg-preview.png" className="absolute right-8 rounded-lg bg-neutral top-4 w-64 h-32"></Logo>
             <div className="h-4/5 w-2/6 bg-secondary flex flex-col gap-12 text-center justify-center items-center">
             <h2 className="font-extrabold flex items-center text-4xl gap-2">
                 <img src="src\assets\genetic-test-report-svgrepo-com.png" className="w-16"></img>
                 ABVdb&Config
             </h2>
                 <img src="src\assets\login-background.jpg" alt="" />
-            <h2 className="font-extrabold text-4xl">Administração</h2>
+            <h2 className="font-extrabold text-4xl bg-pri">Administração</h2>
             </div>
             <div className="h-4/5 w-2/6 bg-neutral flex flex-col text-center items-center">
                 <Forms></Forms>
