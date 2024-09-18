389ecf2d1c5e2dbd0bb9c39f2332a3a5829b0a4f
verttb <alyssonoliveira456@gmail.com>
2024-09-18 18:46:42 -0300

    Bloco das opções

---
+++ src/components/Bloco.tsx
@@ -0,0 +1,15 @@
+interface BlocoProps{
+    title?: string,
+    className: string,
+    img : string,
+    onClick?: () => void
+}
+
+export const Bloco = ({title, img, className, onClick} : BlocoProps) => {
+    return(
+    <div onClick={onClick} className={`items-center justify-center text-center flex ${className}`}>
+        <h2 className="">{title}</h2>
+        <img className="w-2/5" src={img} alt="" />
+    </div>
+    )
+}
