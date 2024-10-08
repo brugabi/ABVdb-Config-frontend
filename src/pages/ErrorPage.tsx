7d01910510b7c9f0726a1b6ce5d7404a53ccb780
verttb <alyssonoliveira456@gmail.com>
2024-10-08 11:15:59 -0300

    error page inicio

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,10 @@
-export const ErrorPage = () => {
+import { Link } from "react-router-dom"
 
+export const ErrorPage = () => {
     return(
-        <div>Página não encontrada</div>
+        <div className="w-full h-screen flex justify-center items-center flex-col gap-4">
+            <div className="text-8xl text-red-400">Página não encontrada</div>
+            <Link className="text-2xl text-blue-400 hover:text-blue-200 underline" to="/home">Clique Aqui para Voltar</Link>
+        </div>
     )
 }
