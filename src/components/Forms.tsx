2d03824c9475372db7e6c7505db0a64b96007b24
VerttB <alyssonoliveira456@gmail.com>
2025-04-11 08:52:16 -0300

    mudando rota de direcionamento

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -28,7 +28,7 @@ export const Forms = () => {
     console.log(data);
     if (user) {
       console.log("Usuário encontrado");
-      navigate("/home");
+      navigate("/");
     } else {
       alert("Usuário ou senha inválidos");
       reset();
